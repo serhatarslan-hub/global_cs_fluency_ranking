bccd1f103e1631ea7a3c7f46583b7e711a982408
serhatarslan-hub <sarslan@stanford.edu>
2019-07-19 17:11:26 -0700

    Ordered terms in popularity   for sequential comp.

--- downloader.py
+++ downloader.py
@@ -1,78 +1,78 @@
 from selenium import webdriver
 import time
 
-# Reference keyword for comparison
-REFERENCE = {
-    'keyword':'hashTable',
-    'query':'%2Fm%2F03llm'
-}
 # Topic-Googlekey map
+# Keywords are ordered from most popular to least popular worldwide in 2018
+#  Popularity ordering allows to compare relatively similar trends, 
+# so that the precision issue, i.e. <1%, is minimized.
 QUERY_MAP = {
-    'python':'%2Fm%2F05z1_',
-    'react':'%2Fm%2F012l1vxv',
-    'javaScript':'%2Fm%2F02p97',
-    'pVsNpProblem':'%2Fm%2F01txp',
-    'churchTuringThesis':'%2Fm%2F01_3k',
-    'polynomialTimeReduction':'%2Fm%2F01529y',
-    'finiteStateMachine':'%2Fm%2F02ykc',
-    'haltingProblem':'%2Fm%2F03k9j',
     'java':'%2Fm%2F07sbkfb',
-    'forLoop':'%2Fm%2F02d6dm',
-    'string':'%2Fm%2F06x16',
-    'segmentationFault':'%2Fm%2F0746c',
-    'integerOverflow':'%2Fm%2F06qksn',
-    'memoryLeak':'%2Fm%2F04yqh',
-    'stack':'%2Fm%2F01p2sd',
-    'heap':'%2Fm%2F03mwv',
-    'NaN':'%2Fm%2F0d3y6',
-    'mutualExclusion':'%2Fm%2F09637',
-    'c++':'%2Fm%2F0jgqg',
-    'semaphore':'%2Fm%2F015xjd',
-    'hyperTextTransferProtocol':'%2Fm%2F03hgt',
-    'nullPointer':'%2Fm%2F012wkzmt',
-    'regularExpression':'%2Fm%2F06f01',
-    'edgeDetection':'%2Fm%2F01w_8f',
-    'computerVision':'%2Fm%2F01xzx',
-    'imageSegmentation':'%2Fm%2F02jj2w',
-    'garbageCollection':'%2Fm%2F01z4f',
-    'transmissionControlProtocol':'%2Fm%2F07hzk',
-    'TCPcongestionControl':'%2Fm%2F05r707',
-    'database':'%2Fm%2F02bc6',
-    'designThinking':'%2Fm%2F0c5z0t',
-    'heuristicEvaluation':'%2Fm%2F01_11z',
-    'interactionDesign':'%2Fm%2F02lvnm',
-    'polynomialHierarchy':'%2Fm%2F030dw0',
-    'cookLevinTheorem':'%2Fm%2F030tgp',
-    'postCorrespondenceProblem':'%2Fm%2F0h7t4',
-    'markovChainMonteCarlo':'%2Fm%2F01jcrc',
-    'dijkstrasAlgorithm':'%2Fm%2F0cf7t',
-    'countMinSketch':'%2Fm%2F0h94cvy',
-    'spectralGraphTheory':'%2Fm%2F02mlcn',
-    'spectralClustering':'%2Fm%2F0h7nlb1',
-    'cache':'%2Fm%2F01zyw',
-    'interruptHandler':'%2Fm%2F02zgwt',
-    'registerFile':'%2Fm%2F05mw3z',
-    'instructionPipelining':'%2Fm%2F01g5qh',
-    'kmeansClustering':'%2Fm%2F061s_s',
-    'artificialNeuralNetworks':'%2Fm%2F05dhw',
-    'logisticRegression':'%2Fm%2F01h161',
-    'gradientDescent':'%2Fm%2F01cmhh',
-    'deepLearning':'%2Fm%2F0h1fn8h',
-    'imageNet':'%2Fg%2F11_ryl961',
-    'concurrencyControl':'%2Fm%2F01ftm6',
-    'queryOptimization':'%2Fm%2F09fs7f',
+    'python':'%2Fm%2F05z1_',
+    'javaScript':'%2Fm%2F02p97',
     'sql':'%2Fm%2F075st',
-    'twoPhaseLocking':'%2Fm%2F02vk5ky',
-    'bTree':'%2Fm%2F01h9m',
-    'extendibleHashing':'%2Fm%2F026nrq0',
-    'linearHashing':'%2Fm%2F08hkpd',
+    'database':'%2Fm%2F02bc6',
+    'string':'%2Fm%2F06x16',
+    'c++':'%2Fm%2F0jgqg',
+    'react':'%2Fm%2F012l1vxv',
+    'cache':'%2Fm%2F01zyw',
+    'hyperTextTransferProtocol':'%2Fm%2F03hgt',
+    'forLoop':'%2Fm%2F02d6dm',
+    'stack':'%2Fm%2F01p2sd',
+    'regularExpression':'%2Fm%2F06f01',
+    'transmissionControlProtocol':'%2Fm%2F07hzk',
+    'NaN':'%2Fm%2F0d3y6',
+    'artificialNeuralNetwork':'%2Fm%2F05dhw',
+    'deepLearning':'%2Fm%2F0h1fn8h',
+    'heap':'%2Fm%2F03mwv',
+    'hashTable':'%2Fm%2F03llm',
+    'logisticRegression':'%2Fm%2F01h161',
+    'designThinking':'%2Fm%2F0c5z0t',
+    'garbageCollection':'%2Fm%2F01z4f',
+    'nullPointer':'%2Fm%2F012wkzmt',
+    'finiteStateMachine':'%2Fm%2F02ykc',
+    'kmeansClustering':'%2Fm%2F061s_s',
+    'semaphore':'%2Fm%2F015xjd',
+    'segmentationFault':'%2Fm%2F0746c',
+    'computerVision':'%2Fm%2F01xzx',
+    'memoryLeak':'%2Fm%2F04yqh',
+    'dijkstrasAlgorithm':'%2Fm%2F0cf7t',
     'mapReduce':'%2Fm%2F05pp4x',
-    'chernoffBound':'%2Fm%2F037gp9',
-    'lovaszLocalLemma':'%2Fm%2F06w7zz',
-    'matrixCompletion':'%2Fm%2F0jkzd0s',
+    'integerOverflow':'%2Fm%2F06qksn',
+    'typeSystem':'%2Fm%2F01cc54',
+    'gradientDescent':'%2Fm%2F01cmhh',
+    'imageSegmentation':'%2Fm%2F02jj2w',
+    'interactionDesign':'%2Fm%2F02lvnm',
+    'instructionPipelining':'%2Fm%2F01g5qh',
+    'bTree':'%2Fm%2F01h9m',
+    'mutualExclusion':'%2Fm%2F09637',
+    'edgeDetection':'%2Fm%2F01w_8f',
+    'imageNet':'%2Fg%2F11bwhb03wl',
+    'interruptHandler':'%2Fm%2F02zgwt',
+    'markovChainMonteCarlo':'%2Fm%2F01jcrc',
+    'pVsNpProblem':'%2Fm%2F01txp',
+    'queryOptimization':'%2Fm%2F09fs7f',
     'LambdaCalculus':'%2Fm%2F04mg4',
+    'TCPcongestionControl':'%2Fm%2F05r707',
+    'twoPhaseLocking':'%2Fm%2F02vk5ky',
+    'concurrencyControl':'%2Fm%2F01ftm6',
+    'haltingProblem':'%2Fm%2F03k9j',
+    'heuristicEvaluation':'%2Fm%2F01_11z',
+    'spectralClustering':'%2Fm%2F0h7nlb1',
+    'registerFile':'%2Fm%2F05mw3z', 
+    'chernoffBound':'%2Fm%2F037gp9',
+    'churchTuringThesis':'%2Fm%2F01_3k',
     'systemF':'%2Fm%2F039vd_',
-    'logicalRelations':'%2Fm%2F01cc54'
+    'matrixCompletion':'%2Fm%2F0jkzd0s',
+    'spectralGraphTheory':'%2Fm%2F02mlcn',
+    'cookLevinTheorem':'%2Fm%2F030tgp',
+    'extendibleHashing':'%2Fm%2F026nrq0',
+    'postCorrespondenceProblem':'%2Fm%2F0h7t4',
+    'polynomialTimeReduction':'%2Fm%2F01529y',
+    'linearHashing':'%2Fm%2F08hkpd',
+    'countMinSketch':'%2Fm%2F0h94cvy',
+    'polynomialHierarchy':'%2Fm%2F030dw0',
+    'lovaszLocalLemma':'%2Fm%2F06w7zz',
+    'logicalRelations':'%2Fg%2F11clwpd'
 }
 
 # Google Trends base URL
@@ -86,15 +86,18 @@ CLASS_NAME = 'export'
 def run():
     browser = webdriver.Chrome()
 
-    # Get the reference keyword only data first
-    url = makeUrl(REFERENCE['query'])
+    allTerms = list(QUERY_MAP.keys())
+    
+    # Get the first keyword only data as country-wise comparison
+    url = makeUrl(QUERY_MAP[allTerms[0]])
     download(browser, url)
-    time.sleep(1)
 
     # Get comparison data for each keyword 
-    reference_term = REFERENCE['keyword']
-    reference_query = REFERENCE['query']
-    for term, query in QUERY_MAP.items():
+    for i in range(1,len(allTerms)):
+        reference_term = allTerms[i-1]
+        reference_query = QUERY_MAP[reference_term]
+        term = allTerms[i]
+        query = QUERY_MAP[term]
         print( reference_term + ' vs. ' + term )
         url = makeUrl(reference_query, query)
         download( browser, url )
@@ -105,6 +108,7 @@ def run():
 def download(browser, url):
     browser.get(url)
     browser.refresh()
+    time.sleep(3)
     elements = browser.find_elements_by_class_name(CLASS_NAME)
     # There are six download buttons on the page.
     # 0: interest over time
